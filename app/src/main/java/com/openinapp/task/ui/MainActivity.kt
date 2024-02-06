49151259ab1941c4876b6d5103dee257b59ca21c
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2024-02-06 10:00:10 +0530

    Working Bottom navigation

--- app/src/main/java/com/openinapp/task/ui/MainActivity.kt
+++ app/src/main/java/com/openinapp/task/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.openinapp.task.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.openinapp.task.R
+import com.openinapp.task.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // setting up bottom nav clicks
+        binding.bottomNavView.setupWithNavController(findNavController(R.id.navHost_fragment))
+
     }
 }
